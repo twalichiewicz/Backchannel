f5752a61cf09d51573173ecdbbb853b40f40ef93
waliwalu <waliwalu@workspaceos.dev>
2026-07-29 09:48:46 -0700

    version bump

--- HNewhere.user.js
+++ HNewhere.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HNewhere
 // @namespace    https://github.com/twalichiewicz/HNewhere
-// @version      1.4.7
+// @version      1.4.8
 // @license MIT
 // @updateURL    https://raw.githubusercontent.com/twalichiewicz/HNewhere/main/HNewhere.user.js
 // @downloadURL  https://raw.githubusercontent.com/twalichiewicz/HNewhere/main/HNewhere.user.js
